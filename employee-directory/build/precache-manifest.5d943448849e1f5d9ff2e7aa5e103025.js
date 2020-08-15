752612d8256c35b9f605147459c203a0928278f9
Matthew Kinzle <matt.kinzle@gmail.com>
2020-08-15 18:00:50 -0500

    movin

---
+++ employee-directory/build/precache-manifest.5d943448849e1f5d9ff2e7aa5e103025.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d2ddc307b856e97440cd43397bc748a",
+    "url": "/React-Employee-Directory/index.html"
+  },
+  {
+    "revision": "7be48424a4158f2cb7ec",
+    "url": "/React-Employee-Directory/static/css/main.8f42d9eb.chunk.css"
+  },
+  {
+    "revision": "a4f92b462c290fadd739",
+    "url": "/React-Employee-Directory/static/js/2.83442f20.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/React-Employee-Directory/static/js/2.83442f20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7be48424a4158f2cb7ec",
+    "url": "/React-Employee-Directory/static/js/main.9772347f.chunk.js"
+  },
+  {
+    "revision": "d5a550fc769b1b6308d1",
+    "url": "/React-Employee-Directory/static/js/runtime-main.f187368d.js"
+  }
+]);
