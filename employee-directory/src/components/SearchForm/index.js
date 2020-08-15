e04c4ef177749dbdf005d8b63b594279b0b2ae8b
Matthew Kinzle <matt.kinzle@gmail.com>
2020-08-15 15:39:01 -0500

    filtering!

--- employee-directory/src/components/SearchForm/index.js
+++ employee-directory/src/components/SearchForm/index.js
@@ -2,12 +2,11 @@ import React, { useEffect, useState } from "react";
 import "./style.css";
 import Form from 'react-bootstrap/Form'
 import API from '../../utils/API';
-import SearchResults from "./SearchResults";
+import SearchResults from "../../components/SearchResults";
 
 
 // Using the datalist element we can create autofill suggestions based on the props.breeds array
-function SearchForm(props) {
-  console.log(props)
+function SearchForm() {
 
   const [searchEmployee, setSearchEmployee] = useState("");
   const [employees, setEmployees] = useState([]);
@@ -22,37 +21,39 @@ function SearchForm(props) {
     API.loadEmps()
       .then(res => {
         setEmployees(res.data.results)
+        console.log(res.data)
+
       })
       .catch(err => console.log(err));
   }
 
-  function employeeSearches(evt) {
+  function employeeSearch(evt) {
     setSearchEmployee(evt);
     filterEmployee(searchEmployee, employees);
   }
 
   function filterEmployee(evt, list) {
 
-    const filteredList = list.filter(employee => employee.name.first.includes(evt));
+    const filteredList = list.filter(employee => employee.name.first.match(evt));
 
     if (filteredList.length > 0) {
       setEmployees(filteredList);
-
     }
-
   }
 
   return (
-    <Form.Group>
-      <Form.Label>Employee Name:</Form.Label>
-      <Form.Control
-        type="text"
-        placeholder="Type employee name here..."
-        name="searchEmployee"
-        onChange={evt => employeeSearches(evt.target.value)}
-      />
-    </Form.Group>
-    <SearchResults employees={employees} />
+    <>
+      <Form.Group>
+        <Form.Label>Employee Name:</Form.Label>
+        <Form.Control
+          type="text"
+          placeholder="Type employee name here..."
+          name="searchEmployee"
+          onChange={evt => employeeSearch(evt.target.value)}
+        />
+      </Form.Group>
+      <SearchResults employees={employees} />
+    </>
   );
 }
 
--- employee-directory/src/pages/Home.js
+++ employee-directory/src/pages/Home.js
@@ -1,9 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Jumbotron from '../components/Jumbotron';
 import Container from 'react-bootstrap/Container';
 import SearchForm from "../components/SearchForm";
-// import SearchResults from "../components/SearchResults";
-// import API from '../utils/API';
 
 
 function Home() {
