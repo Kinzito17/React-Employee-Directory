6f4a3bad4d3ec35014f748b537d69ad1701a55fe
Matthew Kinzle <matt.kinzle@gmail.com>
2020-08-16 16:08:10 -0500

    Updates

---
+++ precache-manifest.4cff5508c39cdf658592618767b20d3b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "358ecd95c2012161e3506a71903c3ecd",
+    "url": "/React-Employee-Directory/index.html"
+  },
+  {
+    "revision": "518a1c96479b56356e79",
+    "url": "/React-Employee-Directory/static/css/main.8f42d9eb.chunk.css"
+  },
+  {
+    "revision": "b8a3f19ef1649dc34bce",
+    "url": "/React-Employee-Directory/static/js/2.fe47a07a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/React-Employee-Directory/static/js/2.fe47a07a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "518a1c96479b56356e79",
+    "url": "/React-Employee-Directory/static/js/main.42647a16.chunk.js"
+  },
+  {
+    "revision": "d5a550fc769b1b6308d1",
+    "url": "/React-Employee-Directory/static/js/runtime-main.f187368d.js"
+  }
+]);
--- src/utils/API.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const BASEURL = "https://randomuser.me/api/?results=50";
-
-export default {
-  loadEmps: function () {
-    return axios.get(BASEURL);
-  }
-};
